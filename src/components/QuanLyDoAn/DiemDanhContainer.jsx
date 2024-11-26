38c17439561944379873c800e38af172ed192448
Nguyễn Tuấn Kiệt <kiet5454aa@gmail.com>
2024-11-26 17:31:07 +0700

    fisrt commit

--- src/components/QuanLyDoAn/DiemDanhContainer.jsx
+++ src/components/QuanLyDoAn/DiemDanhContainer.jsx
@@ -1,8 +1,11 @@
 import Card from "../../ui/Card";
 import StyledTable from "../../ui/Table";
+import formatVieNamDate, { formatVieNamHour } from "../../utils/FormatDate";
 import DiemDanhButton from "./DiemDanh/DiemDanhButton";
 
-function DiemDanhContainer() {
+function DiemDanhContainer({ doAn }) {
+  const diemDanh1 = doAn.sinhVien1.diemDanh || null;
+  const diemDanh2 = doAn.sinhVien2.diemDanh || null;
   return (
     <div>
       <h5>Điểm danh</h5>
@@ -11,12 +14,38 @@ function DiemDanhContainer() {
         <StyledTable>
           <thead>
             <tr>
-              <td>STT</td>
               <td>Mã sinh viên</td>
               <td>Tên sinh viên</td>
-              <td>Sô buổi điểm danh</td>
+              <td>Ngày</td>
+              <td>Giờ</td>
+              <td>Phòng</td>
+              <td>Ghi chú</td>
             </tr>
           </thead>
+          <tbody>
+            {diemDanh1 &&
+              diemDanh1?.map((dd) => (
+                <tr>
+                  <td colSpan={2}>{doAn.sinhVien1.maSo}</td>
+                  <td colSpan={2}>{doAn.sinhVien1.hoten}</td>
+                  <td colSpan={2}>{formatVieNamDate(dd.ngay)}</td>
+                  <td>{formatVieNamHour(dd.ngay)}</td>
+                  <td>{dd.phong}</td>
+                  <td>{dd.ghiChu}</td>
+                </tr>
+              ))}{" "}
+            {diemDanh2 &&
+              diemDanh12?.map((dd) => (
+                <tr>
+                  <td colSpan={2}>{doAn.sinhVien1.maSo}</td>
+                  <td colSpan={2}>{doAn.sinhVien1.hoten}</td>
+                  <td colSpan={2}>{formatVieNamDate(dd.ngay)}</td>
+                  <td>{formatVieNamHour(dd.ngay)}</td>
+                  <td>{dd.phong}</td>
+                  <td>{dd.ghiChu}</td>
+                </tr>
+              ))}
+          </tbody>
         </StyledTable>
       </Card>
     </div>
