60f1b2872ee8ca6bf6ab052b4a9450eb463e651c
Nguyễn Tuấn Kiệt <kiet5454aa@gmail.com>
2024-11-27 20:39:55 +0700

    first commit

--- src/components/QuanLyThucTap/DiemDanhContainer.jsx
+++ src/components/QuanLyThucTap/DiemDanhContainer.jsx
@@ -5,6 +5,7 @@ import DiemDanhButton from "./DiemDanh/DiemDanhButton";
 
 function DiemDanhContainer({ baoCao }) {
   const diemDanh1 = baoCao?.sinhVien?.diemDanh || null;
+  console.log(diemDanh1);
   return (
     <div>
       <h5>Điểm danh</h5>
@@ -24,17 +25,17 @@ function DiemDanhContainer({ baoCao }) {
           <tbody>
             {diemDanh1 &&
               diemDanh1?.map((dd) => {
-                if (+dd.loai !== 1) return;
-                return (
-                  <tr>
-                    <td>{baoCao.sinhVien.maSo}</td>
-                    <td>{baoCao.sinhVien.hoTen}</td>
-                    <td>{formatVieNamDate(dd.ngay)}</td>
-                    <td>{formatVieNamHour(dd.ngay)}</td>
-                    <td>{dd.phong}</td>
-                    <td>{dd.ghiChu}</td>
-                  </tr>
-                );
+                if (+dd.loai === 1)
+                  return (
+                    <tr>
+                      <td>{baoCao.sinhVien.maSo}</td>
+                      <td>{baoCao.sinhVien.hoTen}</td>
+                      <td>{formatVieNamDate(dd.ngay)}</td>
+                      <td>{formatVieNamHour(dd.ngay)}</td>
+                      <td>{dd.phong}</td>
+                      <td>{dd.ghiChu}</td>
+                    </tr>
+                  );
               })}
           </tbody>
         </StyledTable>
